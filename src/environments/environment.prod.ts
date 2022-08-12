93dd071cad77463ed4a8119176f5a3289b311f80
manuel.gomez <manuel.gomez@semovioaxaca.gob.mx>
2022-08-12 11:34:34 -0500

    cambio de url back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  URIBackProveedores:'http://localhost:11025/api',
+  URIBackProveedores:'http://172.16.10.51:1336/api',
   project_name: "Proveedores",
   project_version: 'v1.0.0'
 };
