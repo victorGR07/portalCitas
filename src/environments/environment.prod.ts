e765d591d92d1051c750e7433e4f075ea65c0491
padronProveedores <108490700+padronProveedores@users.noreply.github.com>
2022-10-20 15:23:40 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  URIBackProveedores:'https://citasproveedoresbk.oaxaca.gob.mx/api',
+  URIBackProveedores:'https://citasproveedoresback.oaxaca.gob.mx/api',
   project_name: "Proveedores",
   project_version: 'v1.0.0'
 };
